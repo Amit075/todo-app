de7e8c7fa99ad50f41eac82ff909d97d401ba4fe
Amit Gusain <amitgusain075@gmail.com>
2024-08-31 20:03:53 +0530

    Controller index method and route define

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(TaskController::class)->prefix('tasks')->group(function(){
+    Route::get('index',"index")->name('todoindex');
 });
